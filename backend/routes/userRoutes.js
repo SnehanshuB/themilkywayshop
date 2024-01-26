817b23a09f49a40399eba549789a544d7334cf6b
SnehanshuB <snehanshub998@gmail.com>
2024-01-27 02:42:04 +0530

    added auth middleware and completed auth user and logout user controllers

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,12 +1,15 @@
 import express from 'express';
-import { authUser, registerUser, logoutUser, getUserProfile, updateUserProfile, getUsers, getUserById, deleteUser, updateUser } from '../controllers/userController.js';
+import { 
+    authUser, registerUser, logoutUser, getUserProfile, updateUserProfile, getUsers, getUserById, deleteUser, updateUser 
+} from '../controllers/userController.js';
+import { protect, admin } from '../middleware/authMiddleware.js';
 
 const router = express.Router();
 
-router.route('/').post(registerUser).get(getUsers);
+router.route('/').post(registerUser).get(protect, admin, getUsers);
 router.post('/logout', logoutUser);
-router.post('/login', authUser);
-router.route('/profile').get(getUserProfile).put(updateUserProfile);
-router.route('/:id').get(getUserById).delete(deleteUser).put(updateUser);
+router.post('/auth', authUser);
+router.route('/profile').get(protect, getUserProfile).put(protect, updateUserProfile);
+router.route('/:id').get(getUserById).delete(protect, admin, deleteUser).put(protect, admin, updateUser);
 
 export default router;
