b051155a20f5c85bf66daa3eb76c7589df8fac2c
SnehanshuB <snehanshub998@gmail.com>
2024-01-27 01:22:43 +0530

    added cart screen

--- frontend/src/components/Message.jsx
+++ frontend/src/components/Message.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 
 const Message = ({variant, children}) => {
   return (
-    <Alert variant={variant}>(children)</Alert>
+    <Alert variant={variant}>{children}</Alert>
   )
 };
 
