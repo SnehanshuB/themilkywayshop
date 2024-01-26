2bbb0cd70aae44eedca6da32bf5cb7448e3e8c72
SnehanshuB <snehanshub998@gmail.com>
2024-01-27 01:55:07 +0530

    added user route and user controller basics

---
+++ backend/controllers/userController.js
@@ -0,0 +1,67 @@
+import asyncHandler from "../middleware/asyncHandler.js"
+import User from "../models/userModel.js"
+
+// @desc    Auth user & get token
+// @route   POST /api/users/login
+// @access  Public
+const authUser = asyncHandler(async(req, res) => {
+    res.send('auth user');
+});
+
+// @desc    Register a new user
+// @route   POST /api/users
+// @access  Public
+const registerUser = asyncHandler(async(req, res) => {
+    res.send('register user');
+});
+
+// @desc    Logout user / clear cookie
+// @route   POST /api/users/logout
+// @access  Private
+const logoutUser = asyncHandler(async(req, res) => {
+    res.send('logout user');
+});
+
+// @desc    Get user profile
+// @route   GET /api/users/profile
+// @access  Private
+const getUserProfile = asyncHandler(async(req, res) => {
+    res.send('get user profile');
+});
+
+// @desc    Update user pro
+// @route   PUT /api/users/profile
+// @access  Private
+const updateUserProfile = asyncHandler(async(req, res) => {
+    res.send('update user profile');
+});
+
+// @desc    Get all users
+// @route   GET /api/users/profile
+// @access  Private/Admin
+const getUsers = asyncHandler(async(req, res) => {
+    res.send('get all users');
+});
+
+// @desc    Get user by id
+// @route   GET /api/users/:id
+// @access  Private/Admin
+const getUserById = asyncHandler(async(req, res) => {
+    res.send('get user by id');
+});
+
+// @desc    Delete user
+// @route   DELETE /api/users/:id
+// @access  Private/Admin
+const deleteUser = asyncHandler(async(req, res) => {
+    res.send('delete user');
+});
+
+// @desc    Update user by id
+// @route   PUT /api/users/:id
+// @access  Private/Admin
+const updateUser = asyncHandler(async(req, res) => {
+    res.send('update user by id');
+});
+
+export { authUser, registerUser, logoutUser, getUserProfile, updateUserProfile, getUsers, getUserById, deleteUser, updateUser };
